c5b294396d27608cc0165314440bd64776e53241
alexander.polosatov <alexander.polosatov@devexpress.com>
2023-12-10 12:24:52 +0400

    rework build script

--- build_scripts/prepare_data.mjs
+++ build_scripts/prepare_data.mjs
@@ -1,49 +1,45 @@
 import { join } from 'path';
 import { writeFileSync, readFileSync } from 'fs';
+import { createIfNotExist, getAllDirs } from './utils/utils.mjs';
 
-import { createIfNotExist } from './utils/utils.mjs';
-
-const projectsInfo = [{
-    projectPath: './projects/atm_data',
-    fileName: 'data.json'
-}, {
-    projectPath: './projects/regions_data',
-    fileName: 'data.json'
-}];
+const STATIC_DATA_FOLDER = './static_data';
+const RAW_DATA_FILE_NAME = 'raw_data.json';
+const DIST_DATA_FILE_NAME = 'data.json';
 
 function removeSpacesAndNewlinesOutsideQuotes(inputFilePath, outputFilePath) {
     try {
         const content = readFileSync(inputFilePath, 'utf-8');
 
         const processedContent = content.replace(/("[^"]*")|\s/g, (match, group1) => {
-            if(group1) {
+            if (group1) {
                 return group1;
             }
             return '';
         });
 
         writeFileSync(outputFilePath, processedContent, 'utf-8');
-        console.log('Data prepared');
+        console.log(`Data of ${inputFilePath} file prepared`);
     } catch (error) {
         console.error('Error:', error.message);
     }
 }
 
-function prepareProjects(projects) {
-    projects.forEach(project => {
-        const distFolder = join(project.projectPath, 'dist');
-    
+function prepareProjects(projectsFolders) {
+    projectsFolders.forEach(projectFolder => {
+        const distFolder = join(projectFolder, 'dist');
+
         createIfNotExist(distFolder);
-        const inputFilePath = join(project.projectPath, project.fileName);
-        const outputFilePath = join(distFolder, project.fileName);
-    
-    
+        const inputFilePath = join(projectFolder, RAW_DATA_FILE_NAME);
+        const outputFilePath = join(distFolder, DIST_DATA_FILE_NAME);
+
         removeSpacesAndNewlinesOutsideQuotes(inputFilePath, outputFilePath);
     });
 }
 
 function main() {
-    prepareProjects(projectsInfo);
+    const allProjectsFolders = getAllDirs(STATIC_DATA_FOLDER);
+
+    prepareProjects(allProjectsFolders);
 }
 
 main();
--- build_scripts/utils/utils.mjs
+++ build_scripts/utils/utils.mjs
@@ -1,7 +1,20 @@
-import { existsSync, mkdirSync } from 'fs';
+import { existsSync, mkdirSync, readdirSync, lstatSync } from 'fs';
+import { join } from 'path';
+
+const isDirectory = fileName => {
+    return lstatSync(fileName).isDirectory();
+};
 
 export function createIfNotExist(dirPath) {
     if (!existsSync(dirPath)){
         mkdirSync(dirPath, { recursive: true });
     }
+}
+
+export function getAllDirs(path) {
+    const files = readdirSync(path);
+
+    return files.map(fileName => {
+        return join(path, fileName);
+    }).filter(isDirectory);
 }
