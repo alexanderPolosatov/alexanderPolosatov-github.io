c1d3c5bb7b6f2b03d47b422faa02b0fecc3e4637
alexanderPolosatov <e5412tula3737@yandex.ru>
2025-01-18 16:35:55 +0300

    use utils

--- build_scripts/prepare_data.mjs
+++ build_scripts/prepare_data.mjs
@@ -1,48 +1,49 @@
 import { join } from 'path';
 import { writeFileSync, readFileSync } from 'fs';
-import { createIfNotExist, getAllDirs } from './utils/utils.mjs';
+import { getAllDirs } from './utils/utils.mjs';
+import { node } from 'utils';
 
 const STATIC_DATA_FOLDER = './static_data';
 const RAW_DATA_FILE_NAME = 'raw_data.json';
 const DIST_DATA_FILE_NAME = 'data.json';
 
 function uglifyFileContent(content) {
-    const uglifiedContent = content.replace(/("[^"]*")|\s/g, (match, group1) => {
-        if (group1) {
-            return group1;
-        }
-        return '';
-    });
+  const uglifiedContent = content.replace(/("[^"]*")|\s/g, (match, group1) => {
+    if (group1) {
+      return group1;
+    }
+    return '';
+  });
 
-    return uglifiedContent;
+  return uglifiedContent;
 }
 
 function processFile(inputFilePath, outputFilePath) {
-    try {
-        const content = readFileSync(inputFilePath, 'utf-8');
-        const uglifiedContent = uglifyFileContent(content);
+  try {
+    const content = readFileSync(inputFilePath, 'utf-8');
+    const uglifiedContent = uglifyFileContent(content);
 
-        writeFileSync(outputFilePath, uglifiedContent, 'utf-8');
-        console.log(`Data of ${inputFilePath} file prepared`);
-    } catch (error) {
-        console.error('Error:', error.message);
-    }
+    writeFileSync(outputFilePath, uglifiedContent, 'utf-8');
+    console.log(`Data of ${inputFilePath} file prepared`);
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
 }
 
 function prepareProjects(projectsFolders) {
-    projectsFolders.forEach(projectFolder => {
-        const distFolder = join(projectFolder, 'dist');
+  projectsFolders.forEach(projectFolder => {
+    const distFolder = join(projectFolder, 'dist');
 
-        createIfNotExist(distFolder);
-        const inputFilePath = join(projectFolder, RAW_DATA_FILE_NAME);
-        const outputFilePath = join(distFolder, DIST_DATA_FILE_NAME);
+    node.createFolderRecursiveIfNoExist(distFolder);
+    const inputFilePath = join(projectFolder, RAW_DATA_FILE_NAME);
+    const outputFilePath = join(distFolder, DIST_DATA_FILE_NAME);
 
-        processFile(inputFilePath, outputFilePath);
-    });
+    processFile(inputFilePath, outputFilePath);
+  });
 }
 
 (function main() {
-    const allProjectsFolders = getAllDirs(STATIC_DATA_FOLDER);
+  const allProjectsFolders = getAllDirs(STATIC_DATA_FOLDER);
 
-    prepareProjects(allProjectsFolders);
+  prepareProjects(allProjectsFolders);
 })();
--- build_scripts/utils/utils.mjs
+++ build_scripts/utils/utils.mjs
@@ -1,20 +1,11 @@
-import { existsSync, mkdirSync, readdirSync, lstatSync } from 'fs';
+import { readdirSync } from 'fs';
 import { join } from 'path';
-
-const isDirectory = fileName => {
-    return lstatSync(fileName).isDirectory();
-};
-
-export function createIfNotExist(dirPath) {
-    if (!existsSync(dirPath)){
-        mkdirSync(dirPath, { recursive: true });
-    }
-}
+import { node } from 'utils';
 
 export function getAllDirs(path) {
-    const files = readdirSync(path);
+  const files = readdirSync(path);
 
-    return files.map(fileName => {
-        return join(path, fileName);
-    }).filter(isDirectory);
+  return files.map(fileName => {
+    return join(path, fileName);
+  }).filter(node.isDirectory);
 }
